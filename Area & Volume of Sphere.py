ca95419bb1b5b99e810bd22a5b1c8d921fb229b7
Mohammad Abdullaha Al Forhad <119785564+foorhad@users.noreply.github.com>
2022-12-26 15:18:26 +0600

    Add files via upload

---
+++ Area & Volume of Sphere.py
@@ -0,0 +1,6 @@
+from math import *
+r=int(input('Enter the radius of shpere: '))
+area_sphere=4*pi*r**2
+volume_sphere=4/3*pi*r**3
+print('Area of sphere=',area_sphere)
+print('Volume of sphere =',volume_sphere)
---
+++ area of Circle.py
@@ -0,0 +1,19 @@
+from math import *
+radius=int(input('Enter the radius of circle: '))
+circle_area=pi*radius**2
+print(circle_area)
+
+##alternative method-1
+
+# import math
+# radius=int(input('Enter the radius of circle: '))
+# circle_area=math.pi*radius**2
+# print(circle_area)
+
+
+##alternative method-2
+
+# radius=int(input('Enter the radius of circle: '))
+# pi=3.1416
+# circle_area=pi*radius**2
+# print(circle_area)
